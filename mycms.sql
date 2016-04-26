09e41ad72f948cf53f86daedaad72bf2fa04c6f7
klenzo <crea2luxe@gmail.com>
2016-04-26 15:38:34 +0200

    Sans la bonne BDD pas top ..

--- mycms.sql
+++ mycms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 22 Avril 2016 à 15:58
+-- Généré le :  Mar 26 Avril 2016 à 13:36
 -- Version du serveur :  5.5.47-0+deb7u1-log
 -- Version de PHP :  5.6.20-1~dotdeb+zts+7.1
 
@@ -38,7 +38,85 @@ CREATE TABLE `cms_conf` (
 
 INSERT INTO `cms_conf` (`confid`, `confslug`, `confcontent`) VALUES
 (1, 'conftemplateweb', 'default'),
-(2, 'conftemplatecockpit', 'default');
+(2, 'conftemplatecockpit', 'default'),
+(3, 'aside_pos', 'left'),
+(4, 'lang_default', 'fr');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cms_lang`
+--
+
+CREATE TABLE `cms_lang` (
+  `langid` int(11) NOT NULL,
+  `langcode` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `langname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `langdir` varchar(5) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `cms_lang`
+--
+
+INSERT INTO `cms_lang` (`langid`, `langcode`, `langname`, `langdir`) VALUES
+(1, 'fr', 'Francais', 'fr');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cms_pages`
+--
+
+CREATE TABLE `cms_pages` (
+  `paid` int(10) UNSIGNED NOT NULL,
+  `paslug` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `patitle` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `patype` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `padesc` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `pakeys` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `pacontent` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `paparent` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `pastatut` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
+  `patime` int(10) UNSIGNED NOT NULL,
+  `paroot` int(11) NOT NULL DEFAULT '0',
+  `paorder` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `cms_pages`
+--
+
+INSERT INTO `cms_pages` (`paid`, `paslug`, `patitle`, `patype`, `padesc`, `pakeys`, `pacontent`, `paparent`, `pastatut`, `patime`, `paroot`, `paorder`) VALUES
+(1, 'home', 'Accueil', 'home', 'Hello, it\'s my home page', 'home, page', 'Hello world !', 0, 1, 1461097120, 1, 0),
+(2, 'login', 'Login', 'login', '', '', '', 0, 1, 1461270894, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cms_users`
+--
+
+CREATE TABLE `cms_users` (
+  `uid` int(10) UNSIGNED NOT NULL,
+  `uname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ufname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `umail` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
+  `upass` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `upseudo` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `uavatar` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `utoken` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ucreat` int(11) NOT NULL,
+  `utime` int(11) NOT NULL,
+  `urang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `cms_users`
+--
+
+INSERT INTO `cms_users` (`uid`, `uname`, `ufname`, `umail`, `upass`, `upseudo`, `uavatar`, `utoken`, `ucreat`, `utime`, `urang`) VALUES
+(1, 'cockpit', 'mycms', 'cockpit@mycms.com', '$2y$07$$2y$07$$2y$07$27502d5ujfJIXhS7MhpnttujBvciLkKmimbznQy', 'SuperAdmin', 'admin.png', '27502d58c6e39aOoLvJVM6bAgjQg5r0ZovQKnsbiJAfYKO', 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -76,29 +154,6 @@ CREATE TABLE `pages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `pages`
---
-
-INSERT INTO `pages` (`paid`, `paslug`, `patitle`, `patype`, `padesc`, `pakeys`, `pacontent`, `paparent`, `pastatut`, `patime`, `paroot`, `paorder`) VALUES
-(1, 'home', 'Accueil', 'home', 'Hello, it\'s my home page', 'home, page', 'Hello world !', 0, 1, 1461097120, 1, 0),
-(2, 'login', 'Login', 'login', '', '', '', 0, 1, 1461270894, 0, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `uid` int(10) UNSIGNED NOT NULL,
-  `uname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `ufname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `umail` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
-  `upseudo` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  `urang` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
 -- Index pour les tables exportées
 --
 
@@ -108,6 +163,28 @@ CREATE TABLE `users` (
 ALTER TABLE `cms_conf`
   ADD PRIMARY KEY (`confid`),
   ADD KEY `confslug` (`confslug`);
+
+--
+-- Index pour la table `cms_lang`
+--
+ALTER TABLE `cms_lang`
+  ADD PRIMARY KEY (`langid`),
+  ADD UNIQUE KEY `lang_code` (`langcode`);
+
+--
+-- Index pour la table `cms_pages`
+--
+ALTER TABLE `cms_pages`
+  ADD PRIMARY KEY (`paid`),
+  ADD KEY `patype` (`patype`);
+
+--
+-- Index pour la table `cms_users`
+--
+ALTER TABLE `cms_users`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `umail` (`umail`),
+  ADD KEY `urang` (`urang`);
 
 --
 -- Index pour la table `medias`
@@ -126,14 +203,6 @@ ALTER TABLE `pages`
   ADD KEY `patype` (`patype`);
 
 --
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`),
-  ADD UNIQUE KEY `umail` (`umail`),
-  ADD KEY `urang` (`urang`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -141,7 +210,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `cms_conf`
 --
 ALTER TABLE `cms_conf`
-  MODIFY `confid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `confid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `cms_lang`
+--
+ALTER TABLE `cms_lang`
+  MODIFY `langid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `cms_pages`
+--
+ALTER TABLE `cms_pages`
+  MODIFY `paid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `cms_users`
+--
+ALTER TABLE `cms_users`
+  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `medias`
 --
@@ -151,12 +235,7 @@ ALTER TABLE `medias`
 -- AUTO_INCREMENT pour la table `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `paid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `paid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
